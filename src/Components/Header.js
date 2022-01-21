16b56d2fdca92a7fd30ec2285686ea43edc43ea1
Felype Henrique <felypehenrique25@gmail.com>
2022-01-21 19:04:10 -0300

    Home Page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 import { AppBar, Container, createTheme, makeStyles, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core';
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import { CryptoState } from '../CryptoContext';
 
 const useStyles = makeStyles(() => ({
     title: {
@@ -17,6 +18,10 @@ const Header = () => {
     const classes = useStyles();
 
     const history = useNavigate();
+
+    const { currency, setCurrency} = CryptoState();
+
+    console.log(currency);
 
     const darkTheme = createTheme({
         palette:{
@@ -44,9 +49,11 @@ const Header = () => {
                     height: 40,
                     marginRight: 15,
                 }}
+                value={currency}
+                onChange={(e) => setCurrency(e.target.value)}
                 >
                   <MenuItem value={"USD"}>USD</MenuItem>
-                  <MenuItem value={"INR"}>INR</MenuItem>
+                  <MenuItem value={"BRL"}>BRL</MenuItem>
                 </Select>
             </Toolbar>
           </Container>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CryptoContext from './CryptoContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <CryptoContext>
+      <App />
+    </CryptoContext>
+ ,
   document.getElementById('root')
 );
 
