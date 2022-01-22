3a9435fafdcf421d4b1304ae1aa6aaf9de8be637
Felype Henrique <felypehenrique25@gmail.com>
2022-01-22 01:53:04 -0300

    Trending Coins Carousel

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,5 +1,6 @@
 import { Container, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
+import Carousel from './Carousel';
 
 const useStyles = makeStyles(()=>({
     banner:{
@@ -49,6 +50,7 @@ const Banner = () => {
             Obtenha todas as informações sobre a sua Criptomoeda favorita
           </Typography>
                 </div>
+                <Carousel/>
             </Container>
         </div>
     )
